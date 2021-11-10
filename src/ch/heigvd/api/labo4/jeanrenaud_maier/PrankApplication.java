10b238384a14f1b220d95bb38e23443c3e57e314
nelsonJnrnd <nelsonjeanrenaud@gmail.com>
2021-11-10 12:05:13 +0100

    parameters

--- src/ch/heigvd/api/labo4/jeanrenaud_maier/PrankApplication.java
+++ src/ch/heigvd/api/labo4/jeanrenaud_maier/PrankApplication.java
@@ -2,9 +2,21 @@ package ch.heigvd.api.labo4.jeanrenaud_maier;
 
 import ch.heigvd.api.labo4.jeanrenaud_maier.smtp_client.*;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class PrankApplication {
 
     public static void main(String[] args) {
-
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+            System.exit(-1);
+        } catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Invalid number of parameters");
+            System.exit(-1);
+        }
     }
 }
