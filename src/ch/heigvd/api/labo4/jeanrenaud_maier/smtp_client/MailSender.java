a9a643d52ac4758834e27419c78a566ebf40aa32
damien <91182145+damienmaier@users.noreply.github.com>
2021-12-05 23:51:24 +0100

    implement smpt client connection

--- src/ch/heigvd/api/labo4/jeanrenaud_maier/smtp_client/MailSender.java
+++ src/ch/heigvd/api/labo4/jeanrenaud_maier/smtp_client/MailSender.java
@@ -1,21 +1,44 @@
 package ch.heigvd.api.labo4.jeanrenaud_maier.smtp_client;
 
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.rmi.server.ExportException;
+
 public class MailSender {
-    private final String smtpServerAdress;
-    private final int stmpServerPort;
+    private Socket socket;
+    private Writer serverWriter;
+    private ServerReplyCodesScanner serverReplyCodesScanner;
 
-    public MailSender(String smtpServerAdress, int stmpServerPort) {
-        this.smtpServerAdress = smtpServerAdress;
-        this.stmpServerPort = stmpServerPort;
-        connect();
+    public MailSender(String smtpServerAdress, int smtpServerPort) throws Exception {
+        socket = new Socket(smtpServerAdress, smtpServerPort);
+        serverWriter = new OutputStreamWriter(socket.getOutputStream());
+        serverReplyCodesScanner = new ServerReplyCodesScanner(socket.getInputStream());
+
+        checkServerReply(220);
+        ehloCommand();
     }
-    private void connect(){
-
+    private void ehloCommand() throws Exception {
+        sendLineToServer("EHLO " + InetAddress.getLocalHost().getHostAddress());
+        checkServerReply(250);
     }
 
     public void sendMail(Mail mail){
     }
     public void close(){
 
+    }
+
+    private void sendLineToServer(String line) throws IOException {
+        serverWriter.write(line + "\r\n");
+        serverWriter.flush();
+    }
+
+    private void checkServerReply(int expectedCode) throws Exception {
+        if (serverReplyCodesScanner.nextCode() != expectedCode) {
+            throw new Exception("Unexpected SMTP server reply");
+        }
     }
 }
--- test/ch/heigvd/api/labo4/jeanrenaud_maier/smtp_client/MailSenderTest.java
+++ test/ch/heigvd/api/labo4/jeanrenaud_maier/smtp_client/MailSenderTest.java
@@ -2,15 +2,65 @@ package ch.heigvd.api.labo4.jeanrenaud_maier.smtp_client;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 class MailSenderTest {
+    private static final int PORT = 4242;
+    private ServerSocket receptionistSocket;
+    private Scanner socketScanner;
+    private Writer socketWriter;
+
+    MailSenderTest() throws IOException {
+    }
 
     @BeforeEach
-    void setUp() {
+    void setUp() throws IOException {
+        receptionistSocket = new ServerSocket(PORT);
     }
 
     @AfterEach
-    void tearDown() {
+    void tearDown() throws IOException {
+        receptionistSocket.close();
+    }
+
+    private void acceptClient() throws IOException {
+        Socket socket = receptionistSocket.accept();
+        socketScanner = new Scanner(socket.getInputStream(), StandardCharsets.UTF_8);
+        socketScanner.useDelimiter("\r\n");
+        socketWriter = new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8);
+
+        socketWriter.write("220 unit testing mock server ready\r\n");
+        socketWriter.flush();
+    }
+
+    @Test
+    void ehloShouldWork() throws IOException {
+        new Thread(() -> {
+            try {
+                new MailSender("localhost", PORT);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+        acceptClient();
+
+        String[] splittedClientCommand = socketScanner.next().split(" ", 2);
+        assertEquals("EHLO", splittedClientCommand[0],
+                "the EHLO command must begin with EHLO");
+        assertTrue(splittedClientCommand[1].length() > 0,
+                "the EHLO command must contain an argument");
+    }
+
+    @Test
+    void sendMailShouldWork() {
+
     }
 }
